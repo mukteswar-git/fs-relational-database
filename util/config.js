7029422eb858ea20e98ad59215b695e8a8332aaa
Mukteswar Tripathy <mukteswart5@gmail.com>
2026-09-10 22:25:06 +0530

    test node environment

--- util/config.js
+++ util/config.js
@@ -1,8 +1,13 @@
 require('dotenv').config()
 
+const DATABASE_URL = process.env.DATABASE_URL
+const TEST_DATABASE_URL = process.env.TEST_DATABASE_URL
+const PORT = process.env.PORT || 3001
+const SECRET = process.env.SECRET
+
 module.exports = {
-  DATABASE_URL: process.env.DATABASE_URL,
-  TEST_DATABASE_URL: process.env.TEST_DATABASE_URL,
-  PORT: process.env.PORT || 3001,
-  SECRET: process.env.SECRET
+  DATABASE_URL,
+  TEST_DATABASE_URL,
+  PORT,
+  SECRET
 }
